e84a7635a955de3a01d14c6f8fc62f6b7680eb2e
Dan Palance <danpalance@gmail.com>
2025-09-25 18:12:11 -0700

    Totally overhauled code so the alignment of the bars is fixed

--- R/driversbytaxa_burst.R
+++ R/driversbytaxa_burst.R
@@ -17,6 +17,7 @@ hs_drivers <- main %>%
   separate_rows(Condensed_var, sep =", ") %>% 
   mutate_at(vars(Condensed_var), list(factor)) %>%
   filter(Condensed_var != "none") %>% 
+  droplevels() %>% # Remove the "none" category in Condensed_var so it doesn't mess with the circular bar plot
   mutate(Drivercat = forcats::fct_collapse(Condensed_var,
                                            abiotic = c("dynamic physical", "static physical"),
                                            biotic = c("ecological", "species attributes", "biogeochem"),
@@ -28,15 +29,19 @@ taxa_df <- hs_drivers %>%
   separate_rows(Taxa, sep=",") %>% 
   filter(Taxa != "many") %>% # remove the one study where it was not possible to delineate taxa (Moran and Kanemoto 2007)
   filter(Taxa != "none") %>% # remove studies that didn't look at taxa
-  mutate(Taxa2 = fct_collapse(Taxa,"Fish"=c("bony fish","cart fish","reef fish","fish"),
-                              "Seabirds"="seabirds",
-                              "Mammals"=c("cetaceans","pinnipeds","fissipeds","sirenians","marine mammals","jaguars","mammals"),
-                              "Reptiles"=c("sea turtles","sea snake","reptiles"),
-                              "Plankton"=c("microalgae","nekton","plankton"),
-                              "Krill"="krill",
-                              "Invertebrates"=c("crustaceans","inverts","mollusks","coral","sponges","seastars","urchins","invertebrates"),
-                              "Plants & Seaweed"=c("macroalgae","plants"),
-                              "Microbes"="microbes")) %>% 
+  mutate(Taxa2 = fct_collapse(Taxa,
+                    "Ectothermic Verts" = c("bony fish","cart fish",
+                                           "reef fish","fish", "sea turtles",
+                                           "sea snake","reptiles"),
+                    "Endothermic Verts" = c("seabirds","cetaceans","pinnipeds",
+                                            "fissipeds","sirenians",
+                                            "marine mammals","jaguars","mammals"),
+                    "Plankton"=c("microalgae","nekton","plankton"),
+                    "Macro Invertebrates"=c("krill","crustaceans","inverts",
+                                            "mollusks","coral","sponges","seastars",
+                                            "urchins","invertebrates"),
+                    "Plants & Seaweed"=c("macroalgae","plants"),
+                    "Microbes"="microbes")) %>% 
   group_by(Condensed_var, Drivercat, Taxa2) %>% 
   count(Taxa2) %>% 
   ungroup() %>% 
@@ -44,81 +49,74 @@ taxa_df <- hs_drivers %>%
   mutate(Total=sum(n)) %>% 
   ungroup() %>% 
   mutate(Taxa2 = as.factor(Taxa2)) 
+
   
-
-
-# Setup empty bar spacers between realms
-empty_bar <- 1
-to_add <- data.frame(matrix(NA,empty_bar*nlevels(taxa_df$Taxa2),ncol(taxa_df)))
+# Set a number of 'empty bar' to add at the end of each group
+empty_bar <- 2
+to_add <- data.frame( matrix(NA, empty_bar*nlevels(taxa_df$Taxa2), ncol(taxa_df)) )
 colnames(to_add) <- colnames(taxa_df)
-to_add$Taxa2 <- rep(levels(taxa_df$Taxa2), each = empty_bar)
+to_add$Taxa2 <- rep(levels(taxa_df$Taxa2), each=empty_bar)
 taxa_df <- rbind(taxa_df, to_add)
-taxa_df$Taxa2 <- as.factor(taxa_df$Taxa2)
-taxa_df <- as.data.frame(taxa_df)
-taxa_df <- taxa_df %>% arrange(Taxa2,-n) # Sort by Taxa2 and then number of obs so the bar graphs are ordered from highest to smallest
+taxa_df <- taxa_df %>% arrange(Taxa2)
 taxa_df$id <- seq(1, nrow(taxa_df))
 
 # Get the name and the y position of each label
 label_data <- taxa_df
 number_of_bar <- nrow(label_data)
-angle <- 90 - 360 * (label_data$id-0.5) / number_of_bar     # subtract 0.5 because so the letter matches the angle of the center bars
-label_data$hjust <- ifelse(angle < -90, 1, 0)
+angle <- 90 - 360 * (label_data$id-0.5) /number_of_bar     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
+label_data$hjust <- ifelse( angle < -90, 1, 0)
 label_data$angle <- ifelse(angle < -90, angle+180, angle)
 
 # prepare a data frame for base lines
 base_data <- taxa_df %>% 
   group_by(Taxa2) %>% 
-  summarize(start=min(id)-0.5, end=max(id)-0.5) %>% 
+  summarize(start=min(id), end=max(id) - empty_bar) %>% 
   rowwise() %>% 
   mutate(title=mean(c(start, end)))
-number_of_bar.base <- nrow(base_data)
-base_data$id <- seq(1, nrow(base_data))
-angle <- 90 - 360 * (base_data$id-0.5) /number_of_bar.base
 
-# Create angle for the labels in the circle using the median value from the grouped bar charts labels
-base_data_angle <- label_data %>% 
-  group_by(Taxa2) %>% 
-  summarise(median = median(angle, na.rm = FALSE)) %>%  
-  rename(angle=median)  
-base_data <- merge(base_data,base_data_angle)
-base_data$hjust <- ifelse(angle < 70, 0.5, 0) # if the angle is less than -70, make it 1, otherwise make it 0
-
-# prepare a data frame for the gray scale bars in between each category of bar plot
+# prepare a data frame for grid (scales)
 grid_data <- base_data
-grid_data$end <- grid_data$start
+grid_data$end <- grid_data$end[ c( nrow(grid_data), 1:nrow(grid_data)-1)] + 1
 grid_data$start <- grid_data$start - 1
-#grid_data <- grid_data[-13,] # remove the scale bars for the area where the scale labels will go
+grid_data <- grid_data[-1,]
 
-# Assemble graph 
-# Make the plot 
-p1 <- ggplot(taxa_df, aes(x=Condensed_var, y=n)) +       
-  geom_bar(aes(x=as.factor(id), y=n, fill=Drivercat), stat="identity", color = "black") + 
+# Make the plot
+p1 <- ggplot(taxa_df, aes(x=as.factor(id), y=n, fill=Taxa2)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
+  
+  geom_bar(aes(x=as.factor(id), y=n, fill=Drivercat), stat="identity") +
   scale_fill_manual(values=c("abiotic"="saddlebrown","biotic"="green4", "anthropogenic"="goldenrod")) +
-  # Add a val=20/15/10/5 lines. I do it at the beginning to make sure barplots are OVER it.
-  geom_segment(data=grid_data, aes(x = end, y = 20, xend = start, yend = 20), colour = "grey", alpha=1, linewidth=0.3, inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 15, xend = start, yend = 15), colour = "grey", alpha=1, linewidth=0.3, inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 10, xend = start, yend = 10), colour = "grey", alpha=1, linewidth=0.3, inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 5, xend = start, yend = 5), colour = "grey", alpha=1, linewidth=0.3, inherit.aes = FALSE ) +
+  
+  
+  # Add a val=100/75/50/25 lines. I do it at the beginning to make sur barplots are OVER it.
+  geom_segment(data=grid_data, aes(x = end, y = 80, xend = start, yend = 80), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 60, xend = start, yend = 60), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 40, xend = start, yend = 40), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 20, xend = start, yend = 20), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
   
   # Add text showing the value of each 100/75/50/25 lines
-  annotate("text", x = rep(max(taxa_df$id),5), y = c(0,5, 10, 15, 20), label = c("0","5", "10", "15", "20") , color="grey", size=3 , angle=0, fontface="bold", hjust=1) +
+  annotate("text", x = rep(max(taxa_df$id),4), y = c(20, 40, 60, 80), label = c("20", "40", "60", "80") , color="grey", size=3 , angle=0, fontface="bold", hjust=1) +
+  
+  geom_bar(aes(x=as.factor(id), y=n, fill=Taxa2), stat="identity", alpha=0.5) +
   ylim(-100,120) +
   theme_minimal() +
-  theme(legend.position = "none",
-        axis.text = element_blank(),
-        axis.title = element_blank(),
-        panel.grid = element_blank(),
-        plot.margin = unit(rep(-1,4),"cm")) +
+  theme(
+    legend.position = "none",
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    panel.grid = element_blank(),
+    plot.margin = unit(rep(-1,4), "cm") 
+  ) +
   coord_polar() + 
-  geom_text(data=label_data, aes(x=id, y=n+10, label=Condensed_var, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=4, angle=label_data$angle, inherit.aes = FALSE ) +
+  geom_text(data=label_data, aes(x=id, y=n+10, label=Condensed_var, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE ) +
   
   # Add base line information
-  geom_segment(data=base_data, aes(x = start, y = -5, xend = end, yend = -5), colour = "black", alpha=0.8, linewidth=0.6 , inherit.aes = FALSE )  +
-  geom_text(data=base_data, aes(x = title, y = -17, label=Taxa2, hjust=hjust), angle = base_data$angle,
-            colour = "black", alpha=0.8, size=3.5, fontface="bold", inherit.aes = FALSE)
+  geom_segment(data=base_data, aes(x = start-0.5, y = -5, xend = end+0.5, yend = -5), colour = "black", alpha=0.8, size=0.6 , inherit.aes = FALSE )  +
+  geom_text(data=base_data, aes(x = title, y = -18, label=Taxa2), hjust=1, colour = "black", alpha=0.8, size=4, fontface="bold", inherit.aes = FALSE)
 
-p2 <- ggplot(taxa_df) +       
-  geom_col(aes(x=as.factor(id), y=Total, fill=Taxa2), col=NA, width=1.5) + 
+
+# Inner bar plot
+p2 <- ggplot(taxa_df, aes(x=as.factor(id), y=n, fill=Taxa2)) +       
+  geom_col(aes(x=as.factor(id), y=Total, fill=Taxa2), , col=NA, width =1.1) + 
   scale_fill_brewer(palette = "Set3") +
   coord_polar() +
   theme_minimal() +
@@ -133,7 +131,7 @@ ggdraw() +
   draw_plot(p2,scale=0.38) +
   draw_plot(p1,scale=1)
 ggsave(file ="Figs/driversbytaxa_burst.svg", scale = 2.5)
-ggsave(file ="Figs/driversbytaxa_burst.png", scale = 2.5)
+#ggsave(file ="Figs/driversbytaxa_burst.png", scale = 2.5)
 
 
 #### Make plots for each taxa broken down by driver components
